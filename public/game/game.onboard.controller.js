d37555af56711482f6d2b0885b5ad8be950d6cc1
Jacob Pariseau <jacob@jacobpariseau.com>
2017-01-14 14:49:57 -0700

    Change nudge text

--- public/game/game.onboard.controller.js
+++ public/game/game.onboard.controller.js
@@ -29,35 +29,45 @@ function OnboardController($scope, Socket, Facebook, FBService, DialogService, G
 
   function checkLoginStatus() {
     console.log('Check login status');
-    return Facebook.getLoginStatus(function(response) {
-      if(response.status === 'connected') {
-        vm.facebook.fbLogin = true;
-        return FBService.getUser('me')
-        .then(function(result) {
-          vm.facebook.name = result.name;
-          vm.facebook.id = result.id;
-          console.log(result);
-          FBService.getProfilePicture()
-          .then(function(pic) {
-            console.log(pic);
-            vm.facebook.picture = pic.data.url;
-          });
-        });
+    return new Promise(function(resolve) {
+      Facebook.getLoginStatus(resolve);
+    })
+    .then(function(response) {
+      if(response.status !== 'connected') {
+        console.log('not connected');
+        vm.facebook = {
+          fbLogin: false,
+          name: 'friend!',
+          id: 0,
+          picture: ''
+        };
+        $scope.$apply();
+        return Promise.reject();
       }
-      vm.facebook.fbLogin = false;
+
+      vm.facebook.fbLogin = true;
+      return Promise.all([
+        FBService.getUser(),
+        FBService.getProfilePicture()
+      ])
+      .then(function(results) {
+        console.log(results);
+        vm.facebook.name = results[0].name;
+        vm.facebook.id = results[0].id;
+        vm.facebook.picture = results[1].data.url;
+        $scope.$apply();
+      });
     });
   }
   function logout() {
     Facebook.logout(function(response) {
-      checkLoginStatus()
-      .then(console.log);
+      checkLoginStatus();
     });
   }
 
   function login() {
     Facebook.login(function(response) {
-      checkLoginStatus()
-      .then(console.log);
+      checkLoginStatus();
     });
   }
 }
